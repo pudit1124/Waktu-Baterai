69a181898eebf5e50b2483c136118f77fbef3075
putrito1124 <125179995+pudit1124@users.noreply.github.com>
2023-04-17 19:36:43 +0700

    Add files via upload

--- banyak baterai.php
+++ banyak baterai.php
@@ -2,11 +2,8 @@
 $persen_sisa_baterai = 7; //Persen % 
 $daya_charger = 10; // Watt
 $kapasitas_baterai = 5000; // mAh
-
-// Menghitung Kapasitas baterai yang tersedia
+//operasi
 $sisa_baterai = $kapasitas_baterai * $persen_sisa_baterai / 100;
-
-// Menghitung waktu yang dibutuhkan
 $waktu = ($kapasitas_baterai - $sisa_baterai) / ($daya_charger * 1000 / 60);
 
 echo "Waktu yang dibutuhkan untuk mengisi baterai ponsel anda yang semulanya  " . $persen_sisa_baterai . "% untuk mencapai 100% adalah " . round($waktu, 2) . " menit atau sekitar " . round($waktu/60, 2) . " jam.";
